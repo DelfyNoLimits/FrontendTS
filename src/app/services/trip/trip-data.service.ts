eafd129ce4b85c57eb9e5625ad7ff7b32f296c64
DelfyNoLimits <nickhardav@gmail.com>
2023-09-25 00:42:22 -0500

    00:42

--- src/app/services/trip/trip-data.service.ts
+++ src/app/services/trip/trip-data.service.ts
@@ -7,7 +7,7 @@ import {TripModel} from "../../models/trip.model";
   providedIn: 'root'
 })
 export class TripDataService {
-  baseURL = "https://tripstoreapi.onrender.com/api/tripstore/v1/trips";
+  baseURL = "http://localhost:8080/api/tripstore/v1/trips";
 
   constructor(private http: HttpClient) { }
 
